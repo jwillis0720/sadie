431081675b3850eebc399fd6ce42d81866c734dc
jwillis0720 <jwillis0720@gmail.com>
2020-12-18 17:17:33 -0600

    sadie refernece changes. Expecting changes

--- sadie/reference/app.py
+++ sadie/reference/app.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-value-for-parameter
 """ Applicaiton for generating annotation references for antibodies"""
 
 # third party
@@ -120,103 +121,10 @@ def make_genebank_files_from_db(verbose, database, outpath):
         click.echo(f"Creating {outpath}")
         os.makedirs(outpath)
 
-    # No reason to use click echo over print except to show e can
     generate_genbank(database, outpath)
     click.echo("Done generating files")
 
 
-@click.command()
-@click.option(
-    "-v",
-    "--verbose",
-    count=True,
-    default=4,
-    help="Vebosity level, ex. -vvvvv for debug level logging",
-)
-@click.option(
-    "--out-path",
-    "-o",
-    type=click.Path(file_okay=False, dir_okay=True, resolve_path=True, writable=True),
-    required=True,
-    help="Specify the output path to dump dataframes",
-)
-@click.option("--imgt_db", type=str, help="Specify the path for imgt_db")
-@click.option("--blast-db", type=str, help="Specify the path for blastdb data")
-@click.option("--aux-path", type=str, help="Specify the path for auxillary data ")
-def make_germline_segments(verbose, out_path, imgt_db, blast_db, aux_path):
-    """Generate germline gene segments file
-
-    This script will generate the germline gene segments that are used in the backend by airr.
-
-    Parameters
-    ----------
-    verbose : str
-        verbosity level, e.g vvv
-    out_path : path
-        the out directory
-    imgt_db : path
-        the path to the imgt database file  (ex IMGT_DB.csv.gz). Generated with make-imgt-datbasej
-    blast_db : path
-        the path to the blast db director
-    aux_path : path
-        the path to the auxillary databasess
-
-    Raises
-    ------
-    Exception
-        Missing IMGT fasta files
-    Exception
-        Missing IMGT db file
-    """
-
-    numeric_level = get_verbosity_level(verbose)
-    logging.basicConfig(level=numeric_level)
-    # No reason to use click echo over print except to show e can
-    click.echo("Logging with level =>{}".format(logging.getLevelName(numeric_level)))
-
-    out_path = os.path.abspath(out_path)
-    if not os.path.exists(out_path):
-        click.echo("No output path exists, creating")
-        os.makedirs(out_path)
-        click.echo("Created output path: {}".format(out_path))
-
-    if not imgt_db:
-        # grab the imgt databases
-        imgt_db = os.path.join(os.path.dirname(__file__), "data/IMGT_REFERENCE.csv.gz")
-    if not os.path.exists(imgt_db):
-        click.echo("No IMGT DB exists, use `parse-imgt-db` to generate")
-        raise Exception("IMGT DB missingsMissing")
-
-    if not blast_db:
-        blast_db = os.path.join(os.path.dirname(__file__), "data/germlines/blastdb/")
-        blast_db_path = os.path.abspath(blast_db)
-        if not os.path.exists(blast_db_path):
-            raise Exception(
-                """
-                The path {} does not exist and may have not been generated.
-                Please use the biopharma-reference module to generate the blast db from IMGT data,
-                and use --blast-db-path to specify it's location (ex germline/blastdb)
-                """.format(
-                    blast_db_path
-                )
-            )
-
-    if not aux_path:
-        aux_path = os.path.join(os.path.dirname(__file__), "data/germlines/aux_data/")
-        aux_path = os.path.abspath(aux_path)
-        if not os.path.exists(aux_path):
-            raise Exception(
-                """
-                The path {} does not exist and may have not been generated.
-                Please use the biopharma-reference module to generate the  aux_datafrom IMGT data,
-                and use --aux-path to specify it's location (ex germline/aux_data)
-                """.format(
-                    aux_path
-                )
-            )
-
-
 if __name__ == "__main__":
-    # make_imgt_database()
-    # make_igblast_reference()
-    make_genebank_files_from_db()
+    make_igblast_reference()
+    # make_genebank_files_from_db()
