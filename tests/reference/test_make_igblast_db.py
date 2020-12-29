ae14f4977164100ea14eeb7f02577138fdc41d6f
jwillis0720 <jwillis0720@gmail.com>
2020-12-28 21:08:59 -0600

    new test

--- tests/reference/test_make_igblast_db.py
+++ tests/reference/test_make_igblast_db.py
@@ -2,8 +2,9 @@ import glob
 import os
 import shutil
 import tempfile
-
+import pandas as pd
 from click.testing import CliRunner
+from pkg_resources import resource_filename
 from sadie.reference import app
 
 split_fastas = [
@@ -113,6 +114,69 @@ split_nhd = [
     "/custom/Ig/internal_data/macaque/macaque_V.nhd",
 ]
 
+known_aux_exceptions = {
+    ("mouse", "imgt", "IGLJ4*01"): "igblast has wrong number of c-term remaining",
+    ("rabbit", "imgt", "IGHJ3*02"): "igblast has wrong reading frame",
+}
+
+
+def fixture_dir(dir):
+    """Helper method for test execution."""
+    return resource_filename(__name__, "fixtures/{}".format(dir))
+
+
+def _test_auxilary_file_structure(tmpdir):
+    # Send aux and internal to compare against IGBLAST internal data and aux data
+    my_aux = []
+    generated_aux_path_files = glob.glob(f"{tmpdir}/*/**/*.aux", recursive=True)
+    for file in generated_aux_path_files:
+        df = pd.read_csv(
+            file,
+            skip_blank_lines=True,
+            delimiter="\t",
+            header=None,
+            names=["gene", "reading_frame", "segment", "cdr3_end", "left_over"],
+        )
+        df.insert(0, "common", os.path.basename(file).split("_")[0])
+        df.insert(1, "db_type", file.split("/")[-4])
+        my_aux.append(df)
+    my_aux = pd.concat(my_aux).reset_index(drop=True).set_index(["common", "db_type", "gene"])
+
+    igblast_aux = []
+    igblast_aux_files = glob.glob(fixture_dir("igblast_aux") + "/**.aux")
+    for file in igblast_aux_files:
+        df = pd.read_csv(
+            file,
+            skip_blank_lines=True,
+            delim_whitespace=True,
+            skiprows=2,
+            header=None,
+            names=["gene", "reading_frame", "segment", "cdr3_end", "left_over"],
+        )
+        df.insert(0, "common", os.path.basename(file).split("_")[0])
+        df.insert(1, "db_type", "imgt")
+        igblast_aux.append(df)
+    igblast_aux = pd.concat(igblast_aux).reset_index(drop=True).set_index(["common", "db_type", "gene"])
+    common_index = my_aux.index.intersection(igblast_aux.index)
+    sadie_missing_aux = igblast_aux.index.difference(my_aux.index)
+    igblast_missing_sadie = my_aux.index.difference(igblast_aux.index)
+
+    my_aux_common_index = my_aux.loc[common_index]
+    igblast_common_index = igblast_aux.loc[common_index]
+    for index in my_aux_common_index.index:
+        try:
+            pd._testing.assert_series_equal(igblast_common_index.loc[index], my_aux_common_index.loc[index])
+        except AssertionError:
+            if index in known_aux_exceptions.keys():
+                print(index, "is known exception exception", known_aux_exceptions[index], "skipping")
+                continue
+            else:
+                # raise again since pandas gives way better info
+                pd._testing.assert_series_equal(
+                    igblast_common_index.loc[index], my_aux_common_index.loc[index], obj=index
+                )
+    return True
+
 
 def test_make_igblast_reference():
     """Confirm the CLI works as expecte"""
@@ -141,6 +205,8 @@ def test_make_igblast_reference():
         assert sorted(aux) == sorted(split_aux)
         nhd = [i.split(os.path.basename(tmpdir))[-1] for i in glob.glob(f"{tmpdir}/*/**/*.nhd", recursive=True)]
         assert sorted(nhd) == sorted(split_nhd)
+
+        assert _test_auxilary_file_structure(tmpdir)
 
     if os.path.exists(tmpdir):
         shutil.rmtree(tmpdir)
