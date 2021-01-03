f803d9c52d07a7017b7cfc77199a1971d9de19f9
Jordan R. Willis <jwillis0720@gmail.com>
2021-01-03 17:25:13 -0600

    commit integration branch

--- tests/integration/airr/test_airr_intergration.py
+++ tests/integration/airr/test_airr_intergration.py
@@ -1,3 +1,23 @@
+import pandas as pd
+from sadie.airr import Airr
+
+
+def test_vs_oas_sampling():
+    """
+    Test airr vs the OAS airr datasets. I have chosen 15K heavy and 10K light to compare against
+    """
+    end_point = "https://sadie.s3.us-east-2.amazonaws.com/integration/OAS_sample_subsample.bz2"
+
+    # uzing bzip for max compression to minimize egress S3 expense
+    df_bz2 = pd.read_csv(end_point, index_col=0).reset_index()
+
+    # Airr api
+    airr_api = Airr(species="human", database="imgt", functional="all")
+    airr_api.run_dataframe(
+        df_bz2,
+    )
+
+
 # def test_cli():
 #     """Confirm the CLI works as expecte"""
 #     runner = CliRunner()
