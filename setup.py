35181789a420d182ba9c0416bcd3567c14e58681
Jordan R. Willis <jwillis0720@gmail.com>
2021-01-28 01:57:47 -0600

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class PyTest(test):
 
 setup(
     name="sadie-antibody",
-    version="0.1.3",
+    version="0.1.4",
     python_requires=">=3.6",
     long_description_content_type="text/markdown",
     description="sadie: The Complete Antibody Library",
