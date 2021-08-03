e04743ed0dde0435fed09ff8f21b97201690891d
Jordan R. Willis <jwillis0720@gmail.com>
2021-08-03 18:21:23 -0500

    Bump version: 0.3.11 → 0.3.12

--- src/sadie/airr/__init__.py
+++ src/sadie/airr/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.11"
+__version__ = "0.3.12"
 
 from .airr import Airr, BadDataSet, BadRequstedFileType, GermlineData
 from .airrtable import AirrTable, LinkedAirrTable
--- src/sadie/anarci/__init__.py
+++ src/sadie/anarci/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.11"
+__version__ = "0.3.12"
 from .anarci import Anarci, AnarciDuplicateIdError
 from .result import AnarciResults
 
