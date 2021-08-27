dc3d8d93762fe6c8d32901b04a580322794b65be
Jordan R. Willis <jwillis0720@gmail.com>
2021-08-27 02:16:04 -0500

    Bump version: 0.3.14 → 0.3.15

--- src/sadie/reference/__init__.py
+++ src/sadie/reference/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.14"
+__version__ = "0.3.15"
 from sadie.reference.reference import Reference
 from sadie.reference.internal_data import make_internal_annotaion_file
 from sadie.reference.igblast_ref import make_igblast_ref_database
