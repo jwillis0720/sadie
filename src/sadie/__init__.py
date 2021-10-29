aa05e54435e020629af085f210c1fcee8e7ae921
Jordan R. Willis <jwillis0720@gmail.com>
2021-10-29 05:09:47 -0500

    Bump version: 0.3.18 → 0.3.19

--- src/sadie/__init__.py
+++ src/sadie/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.3.18"
+__version__ = "0.3.19"
 from . import airr, anarci, antibody, reference
 
 __all__ = ["anarci", "airr", "reference", "antibody"]
--- src/sadie/airr/__init__.py
+++ src/sadie/airr/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.3.18"
+__version__ = "0.3.19"
 
 from .airr import Airr, BadDataSet, BadRequstedFileType, GermlineData
 from .airrtable import AirrTable, LinkedAirrTable
