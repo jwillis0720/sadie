63f9d4fc89b29e0490fd120b94ad0b0451b51a48
tmsincomb <troysincomb@gmail.com>
2022-04-17 11:42:02 -0700

    reverted to unoffical way since this is best to keep logic

--- src/sadie/airr/airrtable/__init__.py
+++ src/sadie/airr/airrtable/__init__.py
@@ -1,3 +1,5 @@
 from .airrtable import AirrTable, LinkedAirrTable
+from .airrseries import AirrSeries
 
-__all__ = ["AirrTable", "LinkedAirrTable"]
+
+__all__ = ["AirrSeries", "AirrTable", "LinkedAirrTable"]
