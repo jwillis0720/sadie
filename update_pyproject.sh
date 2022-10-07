17e7b25f22a7b39ff549a7a592e12afa4374bb80
tmsincomb <troysincomb@gmail.com>
2022-10-07 04:33:43 -0700

    poetry patch

--- update_pyproject.sh
+++ update_pyproject.sh
@@ -1,2 +1,3 @@
 poetry add $( cat requirements.txt )
+# poetry add --optional $( cat requirements-dev.txt )  # could replicate pip [] with this
 poetry add --group dev  $( cat requirements-dev.txt )
