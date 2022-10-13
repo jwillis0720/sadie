304c26fb0292349358ed078d43ba575c976f5abe
tmsincomb <troysincomb@gmail.com>
2022-10-12 19:10:17 -0700

    netlify uses requirements.txt while it was just a ref for us renaming to req-core

--- update_pyproject.sh
+++ update_pyproject.sh
@@ -1,3 +1,3 @@
-poetry add $( cat requirements.txt )
+poetry add $( cat requirements-core.txt )
 # poetry add --optional $( cat requirements-dev.txt )  # could replicate pip [] with this
 poetry add --group dev  $( cat requirements-dev.txt )
