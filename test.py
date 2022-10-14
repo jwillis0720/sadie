4057a8065f5d4eb21441d49eaf540528634ed564
jwillis0720 <jwillis0720@gmail.com>
2022-10-14 00:32:28 +0000

    update some deps

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from sadie.airr import Airr
-
-file = "tests/data/fixtures/airr_tables/dog_igh.tsv.gz"
-dog_df = pd.read_csv(file, sep="\t")
-airr_api = Airr("dog")
-unjoined_df = airr_api.run_dataframe(dog_df, "sequence_id", "sequence")
-print(unjoined_df)
