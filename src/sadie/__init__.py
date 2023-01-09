dbab292c75d8463c28c2f48e9008cf4dc89649fd
tmsincomb <troysincomb@gmail.com>
2023-01-09 10:48:17 -0800

    version bump for internal files

--- src/sadie/__init__.py
+++ src/sadie/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.31"
+__version__ = "v0.4.47"
 from . import airr, numbering, receptor, reference, renumbering
 
 __all__ = ["renumbering", "receptor", "airr", "reference", "numbering"]
