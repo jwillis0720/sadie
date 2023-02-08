39f2ba003d73968ab1f2b40c0977ed49f5a701eb
jwillis0720 <jwillis0720@gmail.com>
2023-02-08 20:17:35 +0000

    bump up v0.5.3 version from workflow

--- src/sadie/__init__.py
+++ src/sadie/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "v0.5.2"
+__version__ = "v0.5.3"
 from . import airr, numbering, receptor, reference, renumbering
 
 __all__ = ["renumbering", "receptor", "airr", "reference", "numbering"]
