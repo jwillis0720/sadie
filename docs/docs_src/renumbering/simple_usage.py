bd8f745b5456d3de345bc58a0dacf9394026de96
Jordan R. Willis <jwillis0720@gmail.com>
2023-07-31 15:17:58 -0500

    updating doc

--- docs/docs_src/renumbering/simple_usage.py
+++ docs/docs_src/renumbering/simple_usage.py
@@ -11,8 +11,7 @@ vrc26_seq = "QKQLVESGGGVVQPGRSLTLSCAASQFPFSHYGMHWVRQAPGKGLEWVASITNDGTKKYHGESVWDR
 renumbering_api = Renumbering(scheme="chothia", region_assign="imgt", run_multiproc=True)
 
 # run sequence and return airr table with sequence_id and sequence
-numbering_table = renumbering_api.run_single("VRC26.27_KT371104_Homo_sapiens_anti-HIV-1_immunoglobulin", vrc26_seq)
+numbering_table = renumbering_api.run_single("VRC26.27", vrc26_seq)
 
 # output object types
-print(type(numbering_table))
-print(isinstance(numbering_table, pd.DataFrame))
+print(numbering_table)
