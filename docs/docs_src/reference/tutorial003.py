57ae256b4f29964b1da4f39899188a512c6e6135
Jordan R. Willis <jwillis0720@gmail.com>
2023-07-31 13:46:11 -0500

    update docs

--- docs/docs_src/reference/tutorial003.py
+++ docs/docs_src/reference/tutorial003.py
@@ -1,16 +1,16 @@
 import tempfile
 
-from sadie.reference import Reference
+from sadie.reference import Reference, References
 
 # create empty reference object
 ref_class = Reference()
 with tempfile.TemporaryDirectory() as tmpdirectory:
     # Add genes one at a time
-    ref_class.add_gene({"species": "human", "gene": "IGHV1-69*01", "database": "imgt"})
-    ref_class.add_gene({"species": "human", "gene": "IGHD3-3*01", "database": "imgt"})
-    ref_class.add_gene({"species": "human", "gene": "IGHJ6*01", "database": "imgt"})
+    ref_class.add_gene({"species": "human", "gene": "IGHV1-69*01", "source": "imgt"})
+    ref_class.add_gene({"species": "human", "gene": "IGHD3-3*01", "source": "imgt"})
+    ref_class.add_gene({"species": "human", "gene": "IGHJ6*01", "source": "imgt"})
 
     # call make_airr database on a path
-    ref_class.make_airr_database(tmpdirectory)
-    # use tempdirectory using AIRR module
-    # ...
+    references = References()
+    references.add_reference("human", ref_class)
+    references.make_airr_database(tmpdirectory)
