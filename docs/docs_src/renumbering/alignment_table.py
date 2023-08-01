4638b98bb2fd7d08c393f309231877b67d5b4523
Jordan Willis <jwillis0720@gmail.com>
2023-07-31 23:47:19 -0500

    Update doc (#206)
    * getting genbank working on cli (#190)
    
    * getting genbank working on cli
    
    * literal not defined
    
    * stage new precommit
    
    * adding space at end of airrtable
    
    * fixing suffixes in sadie output
    
    * adding future annotations
    
    * updating with cl integration
    
    * Update macaque constant (#199)
    
    * bump up v0.5.1 version from workflow
    
    * bump up v0.5.2 version from workflow
    
    * getting genbank working on cli (#190) (#191)
    
    * getting genbank working on cli
    
    * literal not defined
    
    * stage new precommit
    
    * adding space at end of airrtable
    
    * fixing suffixes in sadie output
    
    * adding future annotations
    
    * updating with cl integration
    
    * bump up v0.5.3 version from workflow
    
    * changing igl contrived to warning
    
    * changing igl contrived to warning
    
    * changed capture to a warning.
    
    * precommit isort order
    
    * small change to kickoff PR (#192)
    
    * bump up v0.5.4 version from workflow
    
    * HOT-PATCH: sync poetry python version
    
    We could use `poetry env use 3.10`, but it would be a foreseeable bug when poetry no longer supports it or if we no longer support 3.10 .
    
    * hot patch didnt tigger pr so comment and links will do
    
    * favoring matrix to avoid version conflicts from a range
    
    * seperated version bump from publish
    
    * bump up v0.5.8 version from workflow
    
    * none-any already set; 3.x not needed
    
    * bump up v0.5.9 version from workflow
    
    * updating doc for M1 m2 macs
    
    * updating macaque igM
    
    ---------
    
    Co-authored-by: Troy Sincomb <troysincomb@gmail.com>
    
    * renumbering docs init
    
    * mdx-include 1.4.2 not aval for each python version
    
    * update docs
    
    * update doc
    
    * updating doc
    
    * update renumber alignment
    
    * alignment table works
    
    * awesome usuage
    
    ---------
    
    Co-authored-by: Troy Sincomb <troysincomb@gmail.com>

--- docs/docs_src/renumbering/alignment_table.py
+++ docs/docs_src/renumbering/alignment_table.py
@@ -1,24 +1,22 @@
 # Use Renumbering module
-# import pandas for dataframe handling
-import pandas as pd
-from sadie.renumbering.result import NumberingResults
 from sadie.renumbering import Renumbering
+import pandas as pd
 
 # define a single sequence
 vrc26_seq = "QKQLVESGGGVVQPGRSLTLSCAASQFPFSHYGMHWVRQAPGKGLEWVASITNDGTKKYHGESVWDRFRISRDNSKNTLFLQMNSLRAEDTALYFCVRDQREDECEEWWSDYYDFGKELPCRKFRGLGLAGIFDIWGHGTMVIVS"
 
 
-# wrap in a function so we can use multiprocessing
-def run() -> NumberingResults:
+# We wrap these in a function so we can use multiprocessing
+def run() -> pd.DataFrame:
     # setup API  object
     renumbering_api = Renumbering(scheme="chothia", region_assign="imgt", run_multiproc=True)
 
     # run sequence and return airr table with sequence_id and sequence
     numbering_table = renumbering_api.run_single("VRC26.27", vrc26_seq)
 
-    # output object types
-    print(numbering_table)
+    # get the handy dandy alignment table
+    return numbering_table.get_alignment_table()
 
 
 if __name__ == "__main__":
-    run()
+    print(run())
