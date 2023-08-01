e102f93fffcaf874f1d56c4388dc475054270bf4
Jordan R. Willis <jwillis0720@gmail.com>
2023-07-31 23:39:42 -0500

    alignment table works

--- docs/docs_src/renumbering/alignment_table.py
+++ docs/docs_src/renumbering/alignment_table.py
@@ -1,15 +1,22 @@
 # Use Renumbering module
 from sadie.renumbering import Renumbering
-from multiprocessing import freeze_support
+import pandas as pd
 
 # define a single sequence
 vrc26_seq = "QKQLVESGGGVVQPGRSLTLSCAASQFPFSHYGMHWVRQAPGKGLEWVASITNDGTKKYHGESVWDRFRISRDNSKNTLFLQMNSLRAEDTALYFCVRDQREDECEEWWSDYYDFGKELPCRKFRGLGLAGIFDIWGHGTMVIVS"
 
-# setup API  object
-renumbering_api = Renumbering(scheme="chothia", region_assign="imgt", run_multiproc=False)
 
-# run sequence and return airr table with sequence_id and sequence
-numbering_table = renumbering_api.run_single("VRC26.27", vrc26_seq)
+# We wrap these in a function so we can use multiprocessing
+def run() -> pd.DataFrame:
+    # setup API  object
+    renumbering_api = Renumbering(scheme="chothia", region_assign="imgt", run_multiproc=True)
 
-# get the handy dandy alignment table
-numbering_table.get_alignment_table()
+    # run sequence and return airr table with sequence_id and sequence
+    numbering_table = renumbering_api.run_single("VRC26.27", vrc26_seq)
+
+    # get the handy dandy alignment table
+    return numbering_table.get_alignment_table()
+
+
+if __name__ == "__main__":
+    print(run())
