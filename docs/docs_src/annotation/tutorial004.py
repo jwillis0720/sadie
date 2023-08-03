8f0f3aaf04afedaedb3f316236b836c96dfc67e2
Jordan R. Willis <jwillis0720@gmail.com>
2023-08-03 14:51:37 -0500

    docs build

--- docs/docs_src/annotation/tutorial004.py
+++ docs/docs_src/annotation/tutorial004.py
@@ -3,10 +3,12 @@ import pandas as pd
 from sadie.airr import AirrTable
 
 # use AirrTable method to convert AirrTable.tsv to an AirrTable object
-airr_table = AirrTable.read_airr("PG9 AIRR.tsv.gz")
+pg9_path = "PG9 AIRR.tsv.gz"
+
+
+airr_table = AirrTable.read_airr(pg9_path)
 print(type(airr_table), isinstance(airr_table, AirrTable))
 
 # or use pandas read_csv method
-airr_table_from_pandas = AirrTable(pd.read_csv("PG9 AIRR.tsv.gz", sep="\t"))
+airr_table_from_pandas = AirrTable(pd.read_csv(pg9_path, sep="\t"))
 print(type(airr_table_from_pandas), isinstance(airr_table_from_pandas, AirrTable))
-print(airr_table_from_pandas == airr_table)
---
+++ scripts/run_docs_snippets.bash
@@ -0,0 +1,17 @@
+set -e
+
+echo "Running docs_src/annotation"
+cd docs/docs_src/annotation
+for src in $(find . -name '*.py'); do
+    echo "Running $src"
+    python $src
+done
+cd ../../../
+
+echo "Running docs_src/reference"
+cd docs/docs_src/reference
+for src in $(find . -name '*.py'); do
+    echo "Running $src"
+    python $src
+done
+cd ../../../
