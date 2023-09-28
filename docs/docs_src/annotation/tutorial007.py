2f9068bc577b8abd416dc7c369360026b00022c4
Caleb Kibet <ckibet@iavi.org>
2023-09-28 16:43:50 +0300

    Fixed missing import

--- docs/docs_src/annotation/tutorial007.py
+++ docs/docs_src/annotation/tutorial007.py
@@ -3,6 +3,7 @@ from Bio import Entrez
 from Bio.Seq import Seq
 from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
 
 from sadie.airr import Airr
 
