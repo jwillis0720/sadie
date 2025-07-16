702df5ef85e63971c4ffea140ea8367bf6fd470c
Caleb Kibet <ckibet@iavi.org>
2025-07-16 11:01:39 +0000

    updated the blastdatabase for rat, mouse, dog, macaque and rabbit and created a script that automates fetching

--- scripts/prepare_imgt_igblast_db.sh
+++ scripts/prepare_imgt_igblast_db.sh
@@ -4,18 +4,44 @@
 set -e
 
 # Set working directory
-mkdir -p imgt_refs && cd imgt_refs
+gcwd=$(dirname "$0")
+echo "Current working directory: $gcwd"
 
-BLASTPATH="../../src/sadie/reference/bin/linux"
+BLASTPATH="${gcwd}/../src/sadie/reference/bin/linux"
+ABS_BLASTPATH="$(cd "$(dirname "$BLASTPATH")" && pwd)/$(basename "$BLASTPATH")"
+echo "$ABS_BLASTPATH"
+
+cd ${gcwd}
+
 #Define IMGT FASTA URLs
+organism="Macaca mulatta"  # Change this to the desired organism,
+# dictionary to change the scientific name to mouse
+# e.g., "Mus musculus" for mouse
+
+declare -A species_dict=(
+  ["Canis lupus familiaris"]="dog"
+  ["Homo sapiens"]="human"
+  ["Macaca mulatta"]="macaque"
+  ["Mus musculus"]="mouse"
+  ["Oryctolagus cuniculus"]="rabbit"
+  ["Rattus norvegicus"]="rat"
+)
+
+# Current working directory
+gcwd=$(pwd)
+
+organism_link=$(echo $organism | sed 's/ /_/g')
+
+mkdir -p ${species_dict[$organism]} && cd ${species_dict[$organism]}
+
 declare -A urls=(
-  [IGHV]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGHV.fasta"
-  [IGHD]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGHD.fasta"
-  [IGHJ]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGHJ.fasta"
-  [IGKV]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGKV.fasta"
-  [IGLV]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGLV.fasta"
-  [IGLJ]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGLJ.fasta"
-  [IGKJ]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/IGKJ.fasta"
+  [IGHV]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGHV.fasta"
+  [IGHD]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGHD.fasta"
+  [IGHJ]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGHJ.fasta"
+  [IGKV]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGKV.fasta"
+  [IGLV]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGLV.fasta"
+  [IGLJ]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGLJ.fasta"
+  [IGKJ]="https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/${organism_link}/IG/IGKJ.fasta"
 )
 
 
@@ -33,12 +59,12 @@ done
 
 
 # Check for required tools
-if ! command -v ../edit_imgt_file.pl &> /dev/null; then
+if ! command -v $gcwd/edit_imgt_file.pl &> /dev/null; then
     echo "[ERROR] edit_imgt_file.pl not found. Please add IgBLAST's bin directory to your PATH."
     exit 1
 fi
 
-if ! command -v $BLASTPATH/makeblastdb &> /dev/null; then
+if ! command -v $ABS_BLASTPATH/makeblastdb &> /dev/null; then
     echo "[ERROR] makeblastdb not found. Please install BLAST+ tools and add to PATH."
     exit 1
 fi
@@ -48,26 +74,31 @@ for gene in "${!urls[@]}"; do
     in_file="${gene}.fasta"
     out_file="${gene}_igblast.fasta"
     echo "Processing $in_file -> $out_file"
-    perl ../edit_imgt_file.pl "$in_file" > "$out_file"
+    perl $gcwd/edit_imgt_file.pl "$in_file" > "$out_file"
 done
 
 echo "[Step 3] Combining files into V, D, J sets..."
-cat IGHV_igblast.fasta IGKV_igblast.fasta IGLV_igblast.fasta > human_V.fasta
-cp IGHD_igblast.fasta human_D.fasta
-cat IGHJ_igblast.fasta IGLJ_igblast.fasta IGKJ_igblast.fasta > human_J.fasta
+cat IGHV_igblast.fasta IGKV_igblast.fasta IGLV_igblast.fasta > ${species_dict[$organism]}_V.fasta
+cp IGHD_igblast.fasta ${species_dict[$organism]}_D.fasta
+cat IGHJ_igblast.fasta IGLJ_igblast.fasta IGKJ_igblast.fasta > ${species_dict[$organism]}_J.fasta
 
-python ../convert_fasta.py human_V.fasta
-python ../convert_fasta.py human_D.fasta
-python ../convert_fasta.py human_J.fasta
+python $gcwd/convert_fasta.py ${species_dict[$organism]}_V.fasta
+python $gcwd/convert_fasta.py ${species_dict[$organism]}_D.fasta
+python $gcwd/convert_fasta.py ${species_dict[$organism]}_J.fasta
 
 echo "[Step 4] Creating BLAST databases..."
-$BLASTPATH/makeblastdb -dbtype nucl -parse_seqids -in human_V.fasta -out igblast_db/human_V
-$BLASTPATH/makeblastdb -dbtype nucl -parse_seqids -in human_D.fasta -out igblast_db/human_D
-$BLASTPATH/makeblastdb -dbtype nucl -parse_seqids -in human_J.fasta -out igblast_db/human_J
+$ABS_BLASTPATH/makeblastdb -dbtype nucl -parse_seqids -in ${species_dict[$organism]}_V.fasta -out igblast_db/${species_dict[$organism]}_V
+$ABS_BLASTPATH/makeblastdb -dbtype nucl -parse_seqids -in ${species_dict[$organism]}_D.fasta -out igblast_db/${species_dict[$organism]}_D
+$ABS_BLASTPATH/makeblastdb -dbtype nucl -parse_seqids -in ${species_dict[$organism]}_J.fasta -out igblast_db/${species_dict[$organism]}_J
 
-cp human_V.fasta igblast_db/human_V.fasta
-cp human_D.fasta igblast_db/human_D.fasta
-cp human_J.fasta igblast_db/human_J.fasta
+cp ${species_dict[$organism]}_V.fasta igblast_db/${species_dict[$organism]}_V.fasta
+cp ${species_dict[$organism]}_D.fasta igblast_db/${species_dict[$organism]}_D.fasta
+cp ${species_dict[$organism]}_J.fasta igblast_db/${species_dict[$organism]}_J.fasta
 
 echo "[✅ DONE] IgBLAST-ready database created in 'igblast_db/' directory."
 ls -lh igblast_db
+
+cp igblast_db/* ${gcwd}/../src/sadie/airr/data/germlines/Ig/blastdb/${species_dict[$organism]}
+cd "$gcwd"
+echo "Current working directory: $(pwd)"
+echo "Script completed successfully."
