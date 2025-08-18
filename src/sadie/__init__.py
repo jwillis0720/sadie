7deeca8034c9205b7df6c285383064386e0504a7
Troy Sincomb <troysincomb@gmail.com>
2025-08-18 16:05:08 -0700

    Update __init__.py #major
    #major

--- src/sadie/__init__.py
+++ src/sadie/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "v2.0.0"
+__version__ = "v1.1.2"
 from . import airr, numbering, receptor, reference, renumbering
 
 __all__ = ["renumbering", "receptor", "airr", "reference", "numbering"]
